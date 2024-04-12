1fc889045d3fe2619dabb8d3ec1ddf3630a3a1de
saravergara7 <sarac.vergaraq@uqvirtual.edu.co>
2024-04-11 20:38:16 -0500

    Actualizado

---
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.poo;
+
+/**
+ * Clase principal
+ * 
+ * @author Área de programación UQ
+ * @since 2024-01
+ * 
+ *        Licencia GNU/GPL V3.0
+ *        (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE)
+ */
+public class App {
+    // CONSTANTES GLOBALEs
+
+    // Precisión al momento de comparar dos número doubles
+    public static final double PRECISION = 0.05;
+
+    // Mínima nota para aprobar un curso
+    public static final double MINIMA_NOTA = 2.95;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
